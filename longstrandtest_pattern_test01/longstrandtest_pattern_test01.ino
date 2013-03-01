df377c926b5d06d9c1a69f56924ff30fbedc231b
James Kong <fishkingsin@gmail.com>
2013-03-01 13:37:25 +0800

    added fade out

--- longstrandtest_pattern_test01/longstrandtest_pattern_test01.ino
+++ longstrandtest_pattern_test01/longstrandtest_pattern_test01.ino
@@ -11,10 +11,11 @@ int nLEDs = 384;
 // Chose 2 pins for output; can be any valid output pins:
 int dataPin  = 2;
 int clockPin = 3;
-float count = 0;
-int r=255,g=0,b=0;
+int  count = 0;
+float r=255,g=0,b=0;
+float targetRed=255,targetGreen=0,targetBlue=0;
 int mode = 0;
-int fade = 10;
+float fade = 0.1;
 // First parameter is the number of LEDs in the strand.  The LED strips
 // are 32 LEDs per meter but you can extend or cut the strip.  Next two
 // parameters are SPI data and clock pins:
@@ -28,10 +29,12 @@ LPD8806 strip = LPD8806(nLEDs);//, dataPin, clockPin);
 // clock = pin B1.  For Leonardo, this can ONLY be done on the ICSP pins.
 //LPD8806 strip = LPD8806(nLEDs);
 int step_ = 16;
-int delay_ = 100;
+//int delay_ = 100;
 int v = 1;
+int acc = 0;
 uint32_t pixels[384];
 int color=0;
+int _delay = 4;
 void setup() {
   // Start up the LED strip
   strip.begin();
@@ -40,12 +43,12 @@ void setup() {
   strip.show();
   Timer1.initialize();
   Timer1.attachInterrupt(callback, 1000000 / 60); // 60 frames/second
-  delay_ = 0;
+//  delay_ = 0;
   Serial.begin(9600); 
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  color = 1;
+  color = 0;
 }
 
 void loop() {
@@ -56,43 +59,51 @@ void loop() {
     {
       //      r = 255;
 
-      //      r =255;
-      //      g = 0;
-      //      b = 0;
+      targetRed    =  255;
+      targetGreen  =  0;
+      targetBlue    =  0;
       color=1;
+      
     }
     else if(thisChar=='2')
     {
-      //      g =255;
-      //      r = 0;
-      //      b = 0;
+      targetGreen = 255;
+      targetRed = 0;
+      targetBlue = 0;
       color=2;
     }
     else if(thisChar=='3')
     {
 
-      color=3;
+      targetRed = 0 ; 
+      targetGreen = 0;
+      targetBlue = 255;
     }
     else if(thisChar=='4')
     {
 
-      color=4;
+      targetRed = targetGreen = targetBlue  =255;
 
     }
     else if(thisChar=='5')
     {
 
-      color=5;
+      targetRed = 255;
+      targetGreen = 255;
+      targetBlue = 0;
     }
     else if(thisChar=='6')
     {
 
-      color = 6;
+      targetRed = 0;
+      targetGreen = 255;
+      targetBlue = 255;
     }
     else if(thisChar=='0')
     {
-
-      color = 0;
+      targetRed = 0;
+      targetGreen = 0;
+      targetBlue = 0;
     }
     else if(thisChar=='-')
     {
@@ -114,101 +125,100 @@ void loop() {
     {
       mode++;
       mode%=4;
-      for(int i=0; i<strip.numPixels(); i++) {
+      int i=0;
+      for(i=0; i<strip.numPixels(); i++) {
 
         strip.setPixelColor(i, 0);
       }
       count=0;
     }
-    else if( thisChar =='d' && delay_>0)
+    else if( thisChar =='d' && _delay>1)
     {
-      delay_-=10;
+      _delay--;
+
     }
     else if( thisChar =='D')
     {
-      delay_+=10;
+      _delay++;
+    }
+    else if (thisChar == 'f')
+    {
+      if(fade>0)fade-=0.01;
+    }
+    else if (thisChar == 'F')
+    {
+      if(fade<1)fade+=0.01;
     }
 
   }
+//  Serial.println(r);
 
-  switch(color)
-  {
-  case 1:
-    if(g>0)g--;
-    else if(b>0)b--;
-    else if(r<255)r++;
-    delay(fade);
-    break;
-  case 2:
-    {
 
-      if(r>0)r--;
-      else if(b>0)b--;
-      else if(g<255)g++;
-      delay(fade);    
-    }
-    break;
-  case 3:
-    {
-
-      if(r>0)r--;
-      else if(g>0)g--;
-      else if(b<255)b++;
-      delay(fade);    
-    }
-    break;
-  case 4:
-    {
-
-      if(r<255)r++;
-      if(g<255)g++;
-      if(b<255)b++;
-      delay(fade);    
-    }
-    break;
-  case 5:
-    {
-
-      if(r>0)r--;
-      else if(g<255)g++;
-      else if(b<255)b++;
-      delay(fade);    
-    }
-    break;
-  case 6:
-    {
-      if(g>0)g--;
-      else if(r<255)r++;
-      else if(b<255)b++;
-      delay(fade);    
-    }
-    break;
-  case 0:
-    {
-
-      if(r>0)r--;
-      else if(g>0)g--;
-      else if(b>0)b--;
-      delay(fade);    
-    }
-    break;
-  }
-//if(mode==0)
-//{
-//  count+=v;
-//
-//  count=int(count)%strip.numPixels();
-//
-//}
+  //if(mode==0)
+  //{
+  //  count+=v;
+  //
+  //  count=int(count)%strip.numPixels();
+  //
+  //}
   //  delay(delay_);
 }
 void callback() {
-//if(mode!=0)
-{
-  count+=v;
+  r -= float(r-targetRed)*fade;
+  g -= float(g-targetGreen)*fade;
+  b -= float(b-targetBlue)*fade;
+  /*switch(color)
+  {
+  case 1:
 
-  count=int(count)%strip.numPixels();
-}
+      if(g>0)g--;
+      if(b>0)b--;
+      if(r<255)r++;
+
+
+    break;
+  case 2:
+      if(r>0)r--;
+      if(b>0)b--;
+      if(g<255)g++;
+
+    break;
+  case 3:
+      if(r>0)r--;
+      if(g>0)g--;
+      if(b<255)b++;
+    break;
+  case 4:
+      if(r<255)r++;
+      if(g<255)g++;
+      if(b<255)b++;
+    break;
+  case 5:
+      if(r>0)r--;
+      if(g<255)g++;
+      if(b<255)b++;
+    break;
+  case 6:
+      if(g>0)g--;
+      if(r<255)r++;
+      if(b<255)b++;
+    break;
+  case 0:
+    if(r>0)r--;
+    if(g>0)g--;
+    if(b>0)b--;
+    break;
+  }
+   */ 
+  acc++;
+  if(acc%_delay==0)
+  {
+    acc=0;
+
+    count+=v;
+
+    count=int(count)%strip.numPixels();
+  }
   if(count==0)
   {
     if(count<0 && v<0)
@@ -217,13 +227,13 @@ void callback() {
     }
 
   }  
-  colorChase(r,g,b, delay_);
+  colorChase(r,g,b);
 }
 
 // Chase one dot down the full strip.  Good for testing purposes.
-void colorChase(uint8_t r,uint8_t g,uint8_t b, uint8_t wait) {
+void colorChase(uint8_t red,uint8_t green,uint8_t blue) {
   int i;
-  uint32_t c  = strip.Color(r,g,b);
+  uint32_t c  = strip.Color(red,green,blue);
   // Start by turning all pixels off:
   //  for(i=0; i<strip.numPixels(); i++) strip.setPixelColor(i, 0);
 
@@ -245,17 +255,21 @@ void colorChase(uint8_t r,uint8_t g,uint8_t b, uint8_t wait) {
       {
         if(i<count)
           strip.setPixelColor(i, c); // Erase pixel, but don't refresh!
+        else
+          strip.setPixelColor(i, 0);
       }
       else
       {
         if(i>count)
           strip.setPixelColor(i, c); // Erase pixel, but don't refresh!
+        else
+          strip.setPixelColor(i, 0);
       }
 
     }
     else if(mode == 2)
     {
-      if(abs(count-i)<1)
+      if(count==i)
       {        
         strip.setPixelColor(i, c); // Erase pixel, but don't refresh!
       }
@@ -267,8 +281,10 @@ void colorChase(uint8_t r,uint8_t g,uint8_t b, uint8_t wait) {
   }
 
   strip.show(); // Refresh to turn off last pixel
-  delay(wait);
+
 }
+
+
 
 
 
